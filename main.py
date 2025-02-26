c68117342806e174249c86c2e238b4a3b6b1673c
Aleksandra-zercoder <aleks.kemenova@gmail.com>
2025-02-26 19:23:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, BG_COLOR
-from game_cycle import game_loop
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Тир: Молочная бутылка")
     clock = pygame.time.Clock()
 
-    # Запуск игрового цикла
-    game_loop(screen, clock)
+    running = True
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((30, 30, 30))
+        pygame.display.flip()
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
